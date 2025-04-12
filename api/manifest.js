f29b194463c3b7632681df769f5db71d53420be7
iam-rekt <iam-rekt@users.noreply.github.com>
2025-04-12 22:32:55 +0200

    Moving from development mode to production

---
+++ api/manifest.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    name: "MintNPrint",
+    description: "Mint and print your favorite NFTs",
+    image: "https://mintnprintv1-one.vercel.app/welcome.png",
+    url: "https://mintnprintv1-one.vercel.app",
+    frames: {
+      version: "1",
+      home: "https://mintnprintv1-one.vercel.app"
+    }
+  });
+} 
