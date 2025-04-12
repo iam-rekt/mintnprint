c6d76ef197e8e71d5e04fbdbe80f410afb24a312
iam-rekt <iam-rekt@users.noreply.github.com>
2025-04-12 23:24:52 +0200

    Moving from development mode to production

--- api/manifest.js
+++ api/manifest.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
     image: "https://mintnprintv1-one.vercel.app/welcome.png",
     url: "https://mintnprintv1-one.vercel.app",
     frames: {
-      version: "vNext",
+      version: "next",
       home: "https://mintnprintv1-one.vercel.app"
     }
   });
